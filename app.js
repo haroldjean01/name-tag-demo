1bd92255a374208f4d8cce592be59efca67c5edb
Michelle Ross <miross629@gmail.com>
2022-10-25 16:33:38 -0700

    Add button event and get name input

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ console.log('nameDisplay');
 
 nameDisplay.textContent = 'Michelle';
 nameDisplay.classList.add('red');
+
+// button event
+const updateBtn = document.getElementById('update-btn');
+updateBtn.addEventListener('click', () => {
+    console.log('Button click works!!!!');
+
+    const nameInput = document.getElementById('name-input');
+
+    nameDisplay.textContent = nameInput.ariaValueMax;
+    nameInput.value = '';
+});
